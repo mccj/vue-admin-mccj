c300c2bf57cd679d3aac75405dc564ac69fa5a55
毛才君 <mccj@qq.com>
2021-12-19 23:47:09 +0800

    1

---
+++ src/views/redirect.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Redirect",
+  created() {
+    const { params, query } = this.$route;
+    const { path } = params;
+    this.$router.replace({ path: "/" + path, query });
+  },
+});
+</script>
